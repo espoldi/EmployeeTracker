b16234cca6a2d385f10fb4c280d6901a345f45f6
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-27 19:34:16 -0500

    created table for department

--- schema.sql
+++ schema.sql
@@ -2,7 +2,11 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
-CREATE TABLE departments ();
+CREATE TABLE departments (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE roles ();
 
