5d361119e93c385dae4d0eb99c74f69baed46160
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-27 19:30:42 -0500

    added schema outline

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+USE employee_trackerDB;
+
+CREATE TABLE departments ();
+
+CREATE TABLE roles ();
+
+CREATE TABLE employees ();
